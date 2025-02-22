c78151d1a2bbd895fa54766df7fb79a2ae33ec6b
hohanhi <90816483+sleepynhiho@users.noreply.github.com>
2025-02-22 18:27:15 +0700

    Revert "Merge pull request #3 from sleepynhiho/Lợi"
    This reverts commit 409fec0139d42cabb0907e9906f87080cb1f0db3, reversing
    changes made to c7f5309b0648a7421ac6b70d9b794760da13718e.

--- app/src/main/java/com/forrestgump/ig/ui/screens/profile/MyProfileScreen.kt
+++ app/src/main/java/com/forrestgump/ig/ui/screens/profile/MyProfileScreen.kt
@@ -37,13 +37,10 @@ import coil.compose.AsyncImage
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.ui.platform.LocalContext
-import androidx.navigation.NavController
-import com.forrestgump.ig.ui.navigation.Routes
 
 @Composable
 fun MyProfileScreen(
     uiState: UiState,
-    navController: NavController
 ) {
     // Tạm thời set cứng để demo
     uiState.isLoading = false
@@ -58,9 +55,7 @@ fun MyProfileScreen(
                 ProfileTopBar(
                     title = "__td.tung",
                     onBackClicked = { /* TODO */ },
-                    onMoreClicked = {
-                        navController.navigate(Routes.SettingsScreen.route)
-                    }
+                    onMoreClicked = { /* TODO */ }
                 )
 
                 // Khu vực hiển thị thông tin chính: Avatar, Thống kê, Tên, Bio,...
@@ -370,8 +365,5 @@ fun PostFeed(posts: List<String>, onPostClick: (String) -> Unit) {
 @Preview(showBackground = true, backgroundColor = 0xFF000000)
 @Composable
 fun MyProfileScreenPreview() {
-    MyProfileScreen(
-        uiState = UiState(isLoading = false),
-        navController = TODO()
-    )
+    MyProfileScreen(uiState = UiState(isLoading = false))
 }
